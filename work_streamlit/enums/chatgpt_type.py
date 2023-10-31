ed05fe88275ec1a37a785955e9e540792983187d
shinbot <shinbot@uniontool.co.jp>
2023-10-31 17:38:26 +0900

    update

--- work_streamlit/enums/chatgpt_type.py
+++ work_streamlit/enums/chatgpt_type.py
@@ -5,6 +5,7 @@ from enum import Enum
 class ChatGptType(Enum):
     GPT_4 = "gpt-4"
     GPT_3_5_TURBO = "gpt-3.5-turbo"
+    GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k"
 
     @classmethod
     def to_value_list(cls) -> List[str]:
